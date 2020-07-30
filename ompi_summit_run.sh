9b737f2439ffab78a6c5c8534ac8d12989714058
Ann Marie <AnnMarie.Mahon@colorado.edu>
2020-07-30 12:59:00 -0600

    Update parameters for test run

--- ompi_summit_run.sh
+++ ompi_summit_run.sh
@@ -5,15 +5,16 @@
 #SBATCH --qos=normal
 #SBATCH --partition=shas
 #SBATCH --ntasks=15
-#SBATCH --job-name=Simulation
-#SBATCH --output=simulation.%j.out
+#SBATCH --job-name=Simulation-ompi
+#SBATCH --output=simulation-ompi.%j.out
 #SBATCH --mail-user=annmarie.mahon@lasp.colorado.edu
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
 module purge
-module load intel/17.4
-module load impi/17.3
+module load intel/16.0.3
+module load openmpi/1.10.2
+
 
 ./run.sh -d /scratch/summit/jama3001/am/data -i $SLURM_NTASKS
